bfd054221887ebad428bfe9d30f23750f5432ed7
Guillaume-G9 <ggonthier@live.fr>
2022-05-13 13:15:50 +0200

    folder structure

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from '../../components/MAIN/Header/Header'
 
 const Home = () => {
-  return <div></div>;
-};
+  return( 
+    <div>
+      <Header />
+    </div>
+)};
 
 export default Home;

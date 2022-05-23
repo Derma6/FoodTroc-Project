a4b8ce62d5a6d4d06601f0328d87e5e538c0423d
Guillaume-G9 <ggonthier@live.fr>
2022-05-23 15:46:32 +0200

    responsive and minor fixes

--- front-end/src/components/MAIN/Footer/Footer.js
+++ front-end/src/components/MAIN/Footer/Footer.js
@@ -1,22 +1,63 @@
 import React from 'react';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+
 import 'material-icons/iconfont/material-icons.css';
-import { Link } from 'react-router-dom';
 import './Footer.css'
 
 const Footer = () => {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  function social(e){
+    switch(e.target.alt){
+      case "facebook logo": 
+        window.open('https://facebook.com', '_blank', 'noopener,noreferrer');
+        return null;
+      case "instagram logo": 
+        window.open('https://instagram.com', '_blank', 'noopener,noreferrer');
+        return null;
+      case "twitter logo": 
+        window.open('https://twitter.com', '_blank', 'noopener,noreferrer');
+        return null;
+    }
+  }
+
   return (
     <footer>
       <div className="social-text">
         <div className="social">
-          <Link to="https://www.facebook.com"><img src={require('./Facebook_green.png')} alt={"facebook logo"}/></Link>
-          <Link to="https://www.instagram.com"><img alt={"instagram logo"} src={require('./Instagram_green.png')} /></Link>
-          <Link to="https://www.twitter.com"><img alt={"twitter logo"} src={require('./twitter_green.png')} /></Link>
+         <img 
+          target="_blank"
+          onClick={social} 
+          src={require('./Facebook_green.png')} 
+          alt={"facebook logo"}
+          />
+         <img
+          target="_blank" 
+          onClick={social}  
+          alt={"instagram logo"} 
+          src={require('./Instagram_green.png')} 
+          />
+         <img
+          target="_blank" 
+          onClick={social} 
+          alt={"twitter logo"} 
+          src={require('./twitter_green.png')} 
+          />
         </div>
         <div className="legal-text">
+          <Link to="/signup">ACCUEIL</Link>
           <Link to="/contact">CONTACT</Link>
+          <Link to="/signup">INCRIPTION</Link>
+          <h3 onClick={scrollToTop} className="material-icons scroll-to-top">expand_less</h3>
           {/* <Link to="/cgv">CGV</Link> */}
           {/* <Link to="/cookies">COOKIES</Link> */}
-          <Link to="/blog">BLOG</Link>
+          {/* <Link to="/blog">BLOG</Link> */}
         </div>
       </div>
       <div className="footer-text">
